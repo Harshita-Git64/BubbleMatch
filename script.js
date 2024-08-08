b6570485c97d6c67b05f9e3c9e17a0e09dcfa734
Harshita-Git64 <harshitapaliwal789@gmail.com>
2024-08-08 19:07:40 +0530

    Added sound feature

--- script.js
+++ script.js
@@ -1,66 +1,73 @@
-
-
-function makebubble(){
-    let bbl="";
-    for(var i = 1; i <=224;i++)
-    {
-    var val=Math.floor(Math.random()*10);
-    bbl+=`<div id="bubble">${val}</div>`;
+function makebubble() {
+  let bbl = "";
+  for (var i = 1; i <= 190; i++) {
+    var val = Math.floor(Math.random() * 10);
+    bbl += `<div id="bubble">${val}</div>`;
+  }
+  document.querySelector("#bdy").innerHTML = bbl;
+}
+var counting = 1;
+function timerinterval() {
+  var storeval = setInterval(function () {
+    if (counting > 0) {
+      counting--;
+      document.getElementById("timer").innerHTML = counting;
+      // or --> document.querySelector("#timer").textContent=counting;
+    } else {
+      var audio = document.getElementById("gameOver");
+      audio.play();
+      clearInterval(storeval);
+      document.getElementById("bdy").innerHTML = `
+             <span style="color:rgb(199, 91, 122);  font-family: cursive; font-size:50px;margin-bottom:10px; font-weight: semi-bold;">OOPs!! Game Over ${"🙁"}</span>
+             <span style="font-size:40px; font-family: cursive;">Max Score: ${localStorage.getItem(
+               "maxscore",
+               minval
+             )} </span>
+             <span style="color:Black;font-size:40px; font-family: cursive;">Your Score : ${scores} </span>
+             <button id=btn onClick= "document.location.reload()">Play Again</button>`;
     }
-    document.querySelector('#bdy').innerHTML=bbl;
-}
-var counting=60;
-function timerinterval()
-{
-    var storeval=setInterval(function(){
-        if(counting>0)
-        {
-        counting--;
-        document.getElementById("timer").innerHTML=counting;
-       // or --> document.querySelector("#timer").textContent=counting;
-        }
-        else{
-            clearInterval(storeval);
-            document.getElementById("bdy").innerHTML=`
-            <button id=btn onClick= "document.location.reload()">Play Again !!</button>`;
-            //document.getElementById("btn").addEventListener("click",document.location.reload())
-          //  document.getElementById("btn").onclick=document.location.reload();
-           
-        }
-    },1000);
+  }, 1000);
 }
 
-var num=0;
-function randomHitvalue(){
-    num=Math.floor(Math.random()*10);
-    document.getElementById("hitval").innerHTML=num;
-}
-var scores=0;
-
-function increasescore(){
-    scores+=10;
-    document.getElementById("sccr").innerHTML=scores;
+var num = 0;
+function randomHitvalue() {
+  num = Math.floor(Math.random() * 10);
+  document.getElementById("hitval").innerHTML = num;
 }
 
-   document.getElementById("bdy").addEventListener("click",function(getnum)
-   {
-    var clickednum=Number(getnum.target.textContent);
-   
-if(getnum.target.id==="bubble")
-{
-    if(num === clickednum)
-    {
-     increasescore();
-     randomHitvalue();
-     makebubble();
+var scores = 0;
+function increasescore() {
+  var audio = document.getElementById("scoreIncrease");
+  audio.play();
+
+  scores += 10;
+  document.getElementById("sccr").innerHTML = scores;
+}
+
+var minval = -10;
+function maxScore() {
+  minval = Math.max(scores, minval);
+  if (minval > localStorage.getItem("maxscore", minval)) {
+    localStorage.setItem("maxscore", minval);
+  }
+}
+
+document.getElementById("bdy").addEventListener("click", function (getnum) {
+  var clickednum = Number(getnum.target.textContent);
+  if (getnum.target.id === "bubble") {
+    if (num === clickednum) {
+      increasescore();
+      randomHitvalue();
+      makebubble();
+    } else {
+      var audio = document.getElementById("scoreDescrease");
+      audio.play();
+      scores -= 10;
+      document.getElementById("sccr").innerHTML = scores;
     }
-    else{
-        scores-=10;
-        document.getElementById("sccr").innerHTML=scores;
-    }
-}
-}
-);
+    maxScore();
+  }
+});
 
 timerinterval();
 randomHitvalue();
